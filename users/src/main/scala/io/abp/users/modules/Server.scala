954113d266238ccac1598d0406bfdca0b84fe132
Alexandre Balon-Perin <alexbp13@gmail.com>
2020-05-18 14:29:21 +0900

    remove deprecation warnings

--- users/src/main/scala/io/abp/users/modules/Server.scala
+++ users/src/main/scala/io/abp/users/modules/Server.scala
@@ -20,7 +20,7 @@ import zio.telemetry.opentracing.OpenTracing
 object Server {
   type AppTaskEnv[Env] = Env with UserService[Env] with OpenTracing
 
-  def serve[Env: Tagged](
+  def serve[Env: Tag](
       apiConfig: ApiConfig
   ): RIO[ZEnv with AppTaskEnv[Env], Unit] = {
     type AppTask[A] = RIO[AppTaskEnv[Env], A]
